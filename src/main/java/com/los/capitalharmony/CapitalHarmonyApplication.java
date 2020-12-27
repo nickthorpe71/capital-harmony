33081e39bf960310e353c66402cc8189bc8a8fb9
nickthorpe71 <nickthorpe71@gmail.com>
2020-12-27 08:47:02 -0600

    init commit

---
+++ src/main/java/com/los/capitalharmony/CapitalHarmonyApplication.java
@@ -0,0 +1,13 @@
+package com.los.capitalharmony;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapitalHarmonyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapitalHarmonyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/los/capitalharmony/CapitalHarmonyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.los.capitalharmony;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapitalHarmonyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
